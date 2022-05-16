9c41f4a0fedd6abfdb0ad09c0e8414ff147d4f7d
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2022-05-16 11:10:55 -0400

    Adding tests, refactoring index
    Refactored TIME index from an object with getters to an object of
    constants and individual functions `Object.assign`ed into an export
    object. I did this because I didn't want to mess with the scope of an
    object when referencing the getters, but also I thought it foolish to
    calculate the time constants at run time. Time constants are now a
    separate object with fixed numbers that will be checked by calculations
    during tests. (Note: I wonder if calculations on different machine
    architectures will get different results?)
    
    Began writing tests. Moved the calculation of time constants to a CHECK
    object in index.test. Started writing tests for the utility methods,
    but still need to write test for the calculating functions (timeDiff,
    daysSince, daysFrom, daysBetween).

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,143 @@
-import TIME from "./index";
+import TIME, { invalidDateString, d, t } from "./index";
+
+const { dateFromString, timeFromString } = TIME;
+
+const CHECK = {
+  // JavaScript dates are calculated in milliseconds as a base
+  ms: 1,
+
+  get second() {
+    return 1000 * this.ms;
+  },
+
+  get minute() {
+    return 60 * this.second;
+  },
+
+  get hour() {
+    return 60 * this.minute;
+  },
+
+  get day() {
+    return 24 * this.hour;
+  },
+
+  get week() {
+    return 7 * this.day;
+  },
+
+  get fortnight() {
+    return 14 * this.day;
+  },
+
+  // there is no "standard" month length, but I am rounding to 30 days
+  get month() {
+    return 30 * this.day;
+  },
+
+  get year() {
+    return 365 * this.day;
+  },
+
+  get leapYear() {
+    return 366 * this.day;
+  },
+
+  get olympiad() {
+    return 4 * this.year;
+  },
+
+  get decade() {
+    return 10 * this.year;
+  },
+
+  get jubilee() {
+    return 50 * this.year;
+  },
+
+  get century() {
+    return 100 * this.year;
+  },
+};
+
+const hasProperty = (obj: object | null, prop: string) =>
+  Object.prototype.hasOwnProperty.call(obj, prop);
 
 describe("TIME object", () => {
   test("should be an object", () => {
     expect(typeof TIME).toBe("object");
+  });
+
+  describe("should calculate correct values for constants", () => {
+    test("second", () => expect(CHECK.second).toEqual(TIME.second));
+    test("minute", () => expect(CHECK.minute).toEqual(TIME.minute));
+    test("hour", () => expect(CHECK.hour).toEqual(TIME.hour));
+    test("day", () => expect(CHECK.day).toEqual(TIME.day));
+    test("week", () => expect(CHECK.week).toEqual(TIME.week));
+    test("fortnight", () => expect(CHECK.fortnight).toEqual(TIME.fortnight));
+    test("month", () => expect(CHECK.month).toEqual(TIME.month));
+    test("year", () => expect(CHECK.year).toEqual(TIME.year));
+    test("leapYear", () => expect(CHECK.leapYear).toEqual(TIME.leapYear));
+    test("olympiad", () => expect(CHECK.olympiad).toEqual(TIME.olympiad));
+    test("decade", () => expect(CHECK.decade).toEqual(TIME.decade));
+    test("jubilee", () => expect(CHECK.jubilee).toEqual(TIME.jubilee));
+    test("century", () => expect(CHECK.century).toEqual(TIME.century));
+  });
+
+  test("has expected methods", () => {
+    expect(hasProperty(TIME, "invalidDateString")).toBe(true);
+    expect(hasProperty(TIME, "t")).toBe(true);
+    expect(hasProperty(TIME, "timeFromString")).toBe(true);
+    expect(hasProperty(TIME, "d")).toBe(true);
+    expect(hasProperty(TIME, "dateFromString")).toBe(true);
+    expect(hasProperty(TIME, "timeDiff")).toBe(true);
+    expect(hasProperty(TIME, "daysSince")).toBe(true);
+    expect(hasProperty(TIME, "daysFrom")).toBe(true);
+    expect(hasProperty(TIME, "daysBetween")).toBe(true);
+  });
+});
+
+describe("utility methods", () => {
+  test("invalidDateString", () => {
+    // RFC 2822
+    expect(invalidDateString("Sun Apr 20 1969 04:20:00 GMT-0400")).toBe(false);
+    // ISO 8601
+    expect(invalidDateString("1969-04-20T04:20:00+05:00")).toBe(false);
+    // short
+    expect(invalidDateString("04/20/1969")).toBe(false);
+    // long
+    expect(invalidDateString("20 APR 1969")).toBe(false);
+    expect(invalidDateString("20APR1969")).toBe(false);
+    expect(invalidDateString("20-APR-1969")).toBe(false);
+    expect(invalidDateString("20-APR 1969")).toBe(false);
+    //
+    // invalid strings
+    //
+    expect(invalidDateString("-22088400000")).toBe(true);
+    expect(invalidDateString("20/04/1969")).toBe(true);
+    expect(invalidDateString("6969 69 69 69:69:69")).toBe(true);
+    expect(invalidDateString("foo bar baz 4/20 1969 LOL")).toBe(true);
+    expect(invalidDateString("nope")).toBe(true);
+  });
+
+  describe("d or dateFromString", () => {
+    test("are the same function", () => {
+      expect(d).toEqual(dateFromString);
+    });
+    test("returns an object given a valid date string", () => {
+      const test = d("06 JUNE 2006");
+      expect(typeof test).toBe("object");
+      expect(test?.getTime()).toBe(1149566400000);
+    });
+    test("returns null given and invalid date string", () =>
+      expect(d("6969 69 69 69:69:69")).toEqual(null));
+  });
+
+  describe("t or timeFromString", () => {
+    test("are the same function", () => expect(t).toEqual(timeFromString));
+    test("returns a number of milliseconds given a valid date string", () =>
+      expect(t("6/6/6")).toBe(1149566400000));
+    test("returns undefined given an invalid date string", () =>
+      expect(t("nope")).toBe(undefined));
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,119 @@
-// const t = (d:string) => new Date(d).getTime();
-
-// const daysBetween = (from: string, to: string):number => (t(to) - t(from)) / day;
-
-const TIME = {
-  // JavaScript dates are calculated in milliseconds as a base
-  ms: 1,
-
-  get second() {
-    return this.ms;
-  },
-
-  get minute() {
-    return 60 * this.second;
-  },
-
-  get hour() {
-    return 60 * this.minute;
-  },
-
-  get day() {
-    return 24 * this.hour;
-  },
-
-  get week() {
-    return 7 * this.day;
-  },
-
-  get fortnight() {
-    return 14 * this.day;
-  },
-
-  // there is no "standard" month length, but I am rounding to 30 days
-  get month() {
-    return 30 * this.day;
-  },
-
-  get year() {
-    return 365 * this.day;
-  },
-
-  get leapYear() {
-    return 366 * this.day;
-  },
-
-  get olympiad() {
-    return 4 * this.year;
-  },
-
-  get decade() {
-    return 10 * this.year;
-  },
-
-  get jubilee() {
-    return 50 * this.year;
-  },
-
-  get century() {
-    return 100 * this.year;
-  },
+const CONSTANTS = {
+  humanOffset: 1,
 };
+
+export const T = {
+  second: 1000,
+  minute: 60000,
+  hour: 3.6e6,
+  day: 8.64e7,
+  week: 6.048e8,
+  fortnight: 12.096e8,
+  month: 2.592e9,
+  year: 3.1536e10,
+  leapYear: 3.16224e10,
+  olympiad: 1.26144e11,
+  decade: 3.1536e11,
+  jubilee: 1.5768e12,
+  century: 3.1536e12,
+};
+
+/**
+ * invalidDateString tests the validity of a date string used to create
+ * a JavaScript date.
+ *
+ * @example
+ *  invalidDateString("20 APR 1969 04:20:00Z"); // false
+ *  invalidDateString("not a valid date string"); // true
+ *
+ * @function
+ * @name invalidDateString
+ * @param datestring RFC 2822 timestamp
+ * @returns {boolean} A boolean value representing if the string is a valid timestamp
+ */
+export const invalidDateString = (datestring: string) =>
+  isNaN(Date.parse(datestring));
+
+/**
+ * "d" is shorthand for creating a new Date. This function takes a date string
+ * and returns a Date object or null if the string is an invalid date string;
+ *
+ * @function
+ * @name d
+ * @param datestring RFC 2822 timestamp
+ * @returns {Date | null} Date object or null if invalid datestring
+ */
+export const d = (datestring: string): Date | null =>
+  invalidDateString(datestring) ? null : new Date(datestring);
+
+/**
+ * "t" takes a valid date string, creates a date, and returns the value of
+ * Date.getTime. Which is the number of milliseconds since the ECMAScript
+ * epoch (also the UNIX epoch).
+ *
+ * @function
+ * @name t
+ * @param {string} datestring a valid javascript date string
+ * @returns {number | undefined} Date in milliseconds or null if invalid datestring
+ */
+export const t = (date: string): number | undefined => d(date)?.getTime();
+
+/**
+ * timeDiff is a curried function that takes an number representing "now" in the first call
+ * and takes a Date object in the second call to return the difference in milliseconds between
+ * "now" and the given date. As a curried function, this allows for "now" to be partially applied
+ * for testing or getting the difference between a fixed date.
+ * @function
+ * @name timeDiff
+ * @param {Date} historicDate
+ * @return {number} Number of milliseconds difference between historicDate and now
+ */
+export const timeDiff = (now: number) => (historicDate: Date) =>
+  now - historicDate.getTime();
+
+/**
+ * @function
+ * @name daysSince
+ * @param {Date} when
+ * @returns {number} Number of days since supplied date
+ */
+export const daysSince = (when: Date, now: number = Date.now()) =>
+  Math.floor(timeDiff(now)(when) / T.day) + CONSTANTS.humanOffset;
+
+/**
+ * @function
+ * @name daysFrom
+ * @param {Date} when Date to calculate from now
+ * @returns {number} Number of days until "when" date
+ */
+export const daysFrom = (when: Date) => Math.abs(daysSince(when));
+
+/**
+ * @function
+ * @name daysBetween
+ * @param {string} from RFC 2822 timestamp
+ * @param {string} to RFC 2822 timestamp
+ * @returns {number} Number of days between "from" date and "to" date
+ */
+export const daysBetween = (from: string, to: string): number | null => {
+  if (invalidDateString(from) || invalidDateString(to)) {
+    return null;
+  }
+  // using nullish coalescing since t can return "undefined". This seems very
+  // unlikely since we are peforming the check for invalidDateStrings above.
+  return (t(to) ?? 0 - (t(from) ?? 0)) / T.day;
+};
+
+const TIME = Object.assign(T, {
+  invalidDateString,
+  t,
+  timeFromString: t,
+  d,
+  dateFromString: d,
+  timeDiff,
+  daysSince,
+  daysFrom,
+  daysBetween,
+});
 
 export default TIME;
